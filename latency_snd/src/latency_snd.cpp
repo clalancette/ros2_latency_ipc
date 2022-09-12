4a14a820aa8e1f7addb8bcdb8bb459483487c2cc
Chris Lalancette <clalancette@openrobotics.org>
2022-09-12 16:26:20 -0400

    Make LatencySnd ready to be a component.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- latency_snd/src/latency_snd.cpp
+++ latency_snd/src/latency_snd.cpp
@@ -23,24 +23,25 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 
-using namespace std::chrono_literals;
-
-class LatencySnd : public rclcpp::Node
+class LatencySnd final : public rclcpp::Node
 {
 public:
-  LatencySnd(int runs, int snd_size, int delay)
-  : Node("LatencySnd"), runs_(runs), snd_size_(snd_size), delay_(delay)
+  LatencySnd(const rclcpp::NodeOptions & options)
+  : rclcpp::Node("LatencySnd", options)
   {
+    runs_ = declare_parameter("runs", 100);
+    uint64_t snd_size_kb = declare_parameter("send_size_kb", 64);
+    uint64_t delay_ms_ = declare_parameter("delay_ms", 100);
+
     // log test
     std::cout << "----------------------------------------" << std::endl;
     std::cout << "Runs                    : " << runs_ << std::endl;
-    std::cout << "Message size            : " << snd_size_ << " kB" << std::endl;
-    std::cout << "Message delay           : " << delay_ << " ms" << std::endl;
+    std::cout << "Message size            : " << snd_size_kb << " kB" << std::endl;
+    std::cout << "Message delay           : " << delay_ms_ << " ms" << std::endl;
 
     // create message string
     msg_ = std_msgs::msg::String();
-    snd_size_ *= 1024;
-    msg_.data.resize(snd_size_);
+    msg_.data.resize(snd_size_kb * 1024);
     std::fill(msg_.data.begin(), msg_.data.end(), '#');
 
     // define qos
@@ -50,7 +51,7 @@ public:
     pub_ = create_publisher<std_msgs::msg::String>("ping", qos);
 
     // finally create and start timer for publishing
-    auto timer_delay = std::chrono::milliseconds(delay);
+    auto timer_delay = std::chrono::milliseconds(delay_ms_);
     timer_ = create_wall_timer(timer_delay, std::bind(&LatencySnd::OnPublish, this));
   }
 
@@ -114,8 +115,6 @@ private:
   size_t snd_pkgs_ = 0;
   size_t runs_ = 0;
   const size_t warmups_ = 10;
-  size_t snd_size_ = 0;
-  size_t delay_ = 0;
   uint64_t last_warmup_packet_time_ = 0;
   uint64_t warmup_delay_us_ = 10000;
 };
@@ -124,29 +123,12 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
-  int runs(100);  // number of publications
-  {
-    char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-r");
-    if (cli_option) {
-      runs = std::atoi(cli_option);
-    }
-  }
-  int snd_size(64);  // message size in kB
-  {
-    char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-s");
-    if (cli_option) {
-      snd_size = std::atoi(cli_option);
-    }
-  }
-  int delay(100);  // delay between two publications in ms
-  {
-    char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-d");
-    if (cli_option) {
-      delay = std::atoi(cli_option);
-    }
-  }
+  rclcpp::NodeOptions options;
 
-  rclcpp::spin(std::make_shared<LatencySnd>(runs, snd_size, delay));
+  auto node = std::make_shared<LatencySnd>(options);
+
+  rclcpp::spin(node);
+
   rclcpp::shutdown();
 
   return 0;
